8e181901bf54e5e0784345c5f84480161d27cd58
abirmahmud <2320094@iub.edu.bd>
2025-08-16 13:57:13 +0600

    creat an other 4 fxml(u4-u8,controller.nfh

---
+++ src/main/java/com/example/opp_project_g10/abirm/g5c.java
@@ -0,0 +1,39 @@
+package com.example.opp_project_g10.abirm;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class g5c
+{
+    @javafx.fxml.FXML
+    private Label totalSalesLabel;
+    @javafx.fxml.FXML
+    private Label endDatePicker;
+    @javafx.fxml.FXML
+    private Label startDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox eventFilterComboBox;
+    @javafx.fxml.FXML
+    private ComboBox ticketCategoryFilterComboBox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void shareReportButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportPdfButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateReportButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exportCsvButtonOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/opp_project_g10/abirm/g6c.java
@@ -0,0 +1,36 @@
+package com.example.opp_project_g10.abirm;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class g6c
+{
+    @javafx.fxml.FXML
+    private Label invalidTicketMessageLabel;
+    @javafx.fxml.FXML
+    private ComboBox eventListComboBox;
+    @javafx.fxml.FXML
+    private Label ticketStatusLabel;
+    @javafx.fxml.FXML
+    private Label attendanceCountLabel;
+    @javafx.fxml.FXML
+    private TextField ticketIdField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void flagTicketButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void scanQrButtonOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyTicketButtonOA(ActionEvent actionEvent) {
+    }
+}
