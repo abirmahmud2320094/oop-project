c7d655eb277ff7b79dcfe04c75df0058d6e4d17b
Abir mahmud <2320094@iub.edu.bd>
2025-08-19 04:00:13 +0600

    creat 4 controler class

--- src/main/java/com/example/opp_project_g10/abirm/u2c.java
+++ src/main/java/com/example/opp_project_g10/abirm/u2c.java
@@ -1,10 +1,11 @@
 package com.example.opp_project_g10.abirm;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 
-public class u2c
-{
+public class u2c {
+
     @javafx.fxml.FXML
     private DatePicker eventDP;
     @javafx.fxml.FXML
@@ -18,14 +19,34 @@ public class u2c
     @javafx.fxml.FXML
     private CheckBox soundsystemCHB;
     @javafx.fxml.FXML
-    private ComboBox selectevencombobox;
+    private ComboBox<String> selectevencombobox;
 
     @javafx.fxml.FXML
     public void initialize() {
+        selectevencombobox.setItems(FXCollections.observableArrayList("Concert", "Football", "Drama"));
     }
 
     @javafx.fxml.FXML
     public void savechangesBUTTONonAction(ActionEvent actionEvent) {
-    }
+        if (eventDP.getValue() == null) {
+            errorlabel.setText("Please select an event date!");
+            return;
+        }
 
+        String event = selectevencombobox.getValue();
+        if (event == null || event.isEmpty()) {
+            errorlabel.setText("Please select an event!");
+            return;
+        }
+
+        String teams = teamsTA.getText();
+        boolean sound = soundsystemCHB.isSelected();
+        boolean lighting = lightingsystemCHB.isSelected();
+        boolean security = securitypersonnelCB.isSelected();
+
+        // Demo message
+        errorlabel.setText("Changes saved for " + event + " on " + eventDP.getValue() +
+                "\nTeams: " + teams +
+                "\nSound: " + sound + ", Lighting: " + lighting + ", Security: " + security);
+    }
 }
--- src/main/java/com/example/opp_project_g10/abirm/u3c.java
+++ src/main/java/com/example/opp_project_g10/abirm/u3c.java
@@ -1,15 +1,16 @@
 package com.example.opp_project_g10.abirm;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
-public class u3c
-{
+public class u3c {
+
     @javafx.fxml.FXML
-    private ComboBox chooseeventCB;
+    private ComboBox<String> chooseeventCB;
     @javafx.fxml.FXML
     private CheckBox generalAudienceCHB;
     @javafx.fxml.FXML
@@ -23,9 +24,20 @@ public class u3c
 
     @javafx.fxml.FXML
     public void initialize() {
+        chooseeventCB.setItems(FXCollections.observableArrayList("Concert", "Football", "Drama"));
     }
 
     @javafx.fxml.FXML
     public void confirmZonePlanButtonAction(ActionEvent actionEvent) {
-    }
-}
+        String event = chooseeventCB.getValue();
+        if (event == null || event.isEmpty()) {
+            confirmationorvalidationmessageLabiel.setText("Please select an event!");
+            return;
+        }
+
+        boolean general = generalAudienceCHB.isSelected();
+        boolean staff = staffServiceZoneCHB.isSelected();
+        boolean press = pressZonCHB.isSelected();
+        String restrictions = setZoneRestrictionsTA.getText();
+
+        // Demo message
